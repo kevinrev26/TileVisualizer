31c856f732c7de10e6ae61da168b6c882a642c18
Kevin Rivera <kevinrev26@gmail.com>
2021-06-28 15:27:58 -0600

    Read from file function implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os 
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,6 +11,15 @@ directory_name = "results"
 
 def plot_points(chunks, coord):
 
+    # I consider that if x and y were greater than 16, that should correspond to the next chunk id.
+    # for example, the coordinate 8,16,16 should be 9,0,0
+    if(coord[1] >= 16 or coord[2] >= 16):
+        print("Wrong coordinate, please check: {}".format(coord))
+        return
+
+    if(coord[0] >= chunks*chunks):
+        print("Wrong chunk id, please check: {}".format(coord[0]))
+        return
     
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
@@ -40,14 +50,23 @@ def plot_points(chunks, coord):
     ax.yaxis.tick_left()
 
     title = "{}_{}_{}".format(coord[0],coord[1], coord[2])
-    plt.title("({},{},{})".format(coord[0],coord[1], coord[2]))
+    plt.title("Grid size (chunks): {}x{}, coordinate: ({},{},{})".format(chunks, chunks, coord[0],coord[1], coord[2]))
     plt.plot(x_coord, y_coord, "ro")
     ax.grid(which='minor', alpha=0.2)
     ax.grid(which='major', alpha=0.8)
     plt.savefig("results/{}.png".format(title))
+    print("Saved: {}".format(title))
     #plt.show()
 
-    print("All the values have been saved on {} directory".format(directory_name) )
+    
+
+def read_from_file():
+    with open("data.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=" ", quotechar="|")
+        for row in reader:
+            if row:
+                values = list(map(int, row[0].split(",")))                
+                plot_points(values[0], values[1:4])
 
 if __name__ == "__main__":
     # Create results directory if not exists
@@ -55,8 +74,6 @@ if __name__ == "__main__":
         os.mkdir(directory_name)
         print("Directory created!")
 
-    chunks = input("chunks: ")
-    input_coordinate = input("Enter coordinate (n, x, y): ")
-    
-    coordinates = map(int, input_coordinate.split(","))
-    plot_points(int(chunks), list(coordinates))
+    print("Processing...")
+    read_from_file()
+    print("All the values have been saved on {} directory".format(directory_name) ) 
